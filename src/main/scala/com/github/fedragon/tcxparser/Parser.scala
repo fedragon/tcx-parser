fec24e4de767071cac5682867f3a7d2ee855fd50
Federico Ragona <federico.ragona@gmail.com>
2017-04-22 19:35:41 +0200

    Safely parse TCX files
    wrapping result in `Try`

--- src/main/scala/com/github/fedragon/tcxparser/Parser.scala
+++ src/main/scala/com/github/fedragon/tcxparser/Parser.scala
@@ -1,6 +1,7 @@
 package com.github.fedragon.tcxparser
 
 import org.joda.time.DateTime
+import scala.util.{Failure, Success, Try}
 import scala.xml._
 
 case class Position(
@@ -9,7 +10,7 @@ case class Position(
 
 case class HeartRateBpm(value: Int)
 
-case class Trackpoint(
+case class TrackPoint(
   time: DateTime,
   position: Option[Position],
   altitudeMeters: Option[Double],
@@ -28,7 +29,7 @@ case class Lap(
   intensity: Option[String],
   notes: Option[String],
   triggerMethod: String,
-  track: Seq[Trackpoint])
+  track: Seq[TrackPoint])
 
 case class Activity(sport: String, id: String, laps: Seq[Lap])
 
@@ -36,57 +37,79 @@ case class TrainingCenterDatabase(activities: Seq[Activity])
 
 object Parser {
 
-  def parse(fileName: String): TrainingCenterDatabase =
-    parse(XML.loadFile(fileName))
+  def parse(path: String): Try[TrainingCenterDatabase] =
+    parse(XML.loadFile(path))
 
-  def parse(root: NodeSeq): TrainingCenterDatabase =
-    TrainingCenterDatabase(
-      (root \\ "Activity").map { activity =>
-        Activity(
-          (activity \ "@Sport").text,
-          (activity \ "Id").text,
-          parseLaps(activity \ "Lap"))
-      }
-    )
+  def parse(root: NodeSeq): Try[TrainingCenterDatabase] =
+    sequence((root \\ "Activity").map(parseActivity))
+      .map(TrainingCenterDatabase(_))
 
-  private[tcxparser] def parseLaps(laps: NodeSeq) =
-    laps.map { lap =>
-      Lap(
-        DateTime.parse((lap \ "@StartTime").text),
-        (lap \ "TotalTimeSeconds").text.toDouble,
-        (lap \ "DistanceMeters").text.toDouble,
-        (lap \ "MaximumSpeed").text.toDouble,
-        (lap \ "Calories").text.toLong,
-        optionally(lap \ "AverageHeartRateBpm")(parseHeartRateBpm),
-        optionally(lap \ "MaximumHeartRateBpm")(parseHeartRateBpm),
-        optionally(lap \ "Intensity")(_.text),
-        optionally(lap \ "Notes")(_.text),
-        (lap \ "TriggerMethod").text,
-        parseTrack(lap \ "Track")
-      )
-    }
+  private[tcxparser] def parseActivity(activity: NodeSeq) =
+    for {
+      laps <- sequence((activity \ "Lap").map(parseLap))
+    } yield Activity(
+      (activity \ "@Sport").text,
+      (activity \ "Id").text,
+      laps)
+
+  private[tcxparser] def parseLap(lap: NodeSeq) =
+    for {
+      track <- parseTrack(lap \ "Track")
+      avg <- optionally(lap \ "AverageHeartRateBpm")(parseHeartRateBpm)
+      max <- optionally(lap \ "MaximumHeartRateBpm")(parseHeartRateBpm)
+      intensity <- optionally(lap \ "Intensity")(_.text)
+      notes <- optionally(lap \ "Notes")(_.text)
+      startTime <- Try(DateTime.parse((lap \ "@StartTime").text))
+      totalTime <- Try((lap \ "TotalTimeSeconds").text.toDouble)
+      distance <- Try((lap \ "DistanceMeters").text.toDouble)
+      maxSpeed <- Try((lap \ "MaximumSpeed").text.toDouble)
+      calories <- Try((lap \ "Calories").text.toLong)
+    } yield Lap(
+      startTime,
+      totalTime,
+      distance,
+      maxSpeed,
+      calories,
+      avg,
+      max,
+      intensity,
+      notes,
+      (lap \ "TriggerMethod").text,
+      track)
 
   private[tcxparser] def parseHeartRateBpm(heartRateBpm: NodeSeq) =
     HeartRateBpm((heartRateBpm \ "Value").text.toInt)
 
+  private[tcxparser] def parseTrackpoint(point: NodeSeq) =
+    for {
+      datetime <- Try(DateTime.parse((point \ "Time").text))
+      position <- optionally(point \ "Position")(parsePosition)
+      altitude <- optionally(point \ "AltitudeMeters")(_.text.toDouble)
+      distance <- optionally(point \ "DistanceMeters")(_.text.toDouble)
+      heartRate <- optionally(point \ "HeartRateBpm")(parseHeartRateBpm)
+      sensorState <- optionally(point \ "SensorState")(_.text)
+    } yield TrackPoint(
+      datetime,
+      position,
+      altitude,
+      distance,
+      heartRate,
+      sensorState)
+
   private[tcxparser] def parseTrack(track: NodeSeq) =
-    (track \ "Trackpoint").map { point =>
-      Trackpoint(
-        DateTime.parse((point \ "Time").text),
-        optionally(point \ "Position")(parsePosition),
-        optionally(point \ "AltitudeMeters")(_.text.toDouble),
-        optionally(point \ "DistanceMeters")(_.text.toDouble),
-        optionally(point \ "HeartRateBpm")(parseHeartRateBpm),
-        optionally(point \ "SensorState")(_.text)
-      )
-    }
+    sequence((track \ "Trackpoint").map(parseTrackpoint))
 
   private[tcxparser] def parsePosition(position: NodeSeq) =
     Position(
       (position \ "LatitudeDegrees").text.toDouble,
-      (position \ "LongitudeDegrees").text.toDouble
-    )
+      (position \ "LongitudeDegrees").text.toDouble)
 
   private[tcxparser] def optionally[T](node: NodeSeq)(f: NodeSeq => T) =
-    Option(node).filter(_.nonEmpty).map(f)
+    Try(Option(node).filter(_.nonEmpty).map(f))
+
+  private[tcxparser] def sequence[T](xs: Seq[Try[T]]): Try[Seq[T]] =
+    xs
+      .find(_.isFailure)
+      .map(t => Failure(t.failed.get))
+      .getOrElse(Success(xs.map(_.get)))
 }
--- src/test/scala/com/github/fedragon/tcxparser/ParserSpec.scala
+++ src/test/scala/com/github/fedragon/tcxparser/ParserSpec.scala
@@ -2,6 +2,7 @@ package com.github.fedragon.tcxparser
 
 import org.joda.time.DateTime
 import org.scalatest._
+import scala.util._
 import scala.xml.NodeSeq
 
 class ParserSpec extends FreeSpec with Matchers {
@@ -39,20 +40,20 @@ class ParserSpec extends FreeSpec with Matchers {
             </HeartRateBpm>
             <SensorState>Present</SensorState>
           </Trackpoint>
-        </Track>).head
-      val expected = Trackpoint(
+        </Track>)
+      val expected = Seq(TrackPoint(
         DateTime.parse("2016-04-30T09:38:58.000Z"),
         None,
         Some(3.0),
         Some(100.0),
         Some(HeartRateBpm(87)),
-        Some("Present"))
+        Some("Present")))
 
-      track shouldEqual expected
+      track shouldEqual Success(expected)
     }
 
     "Lap is parsed successfully" in {
-      val laps = parseLaps(
+      val laps = parseLap(
         <Lap StartTime="2016-04-30T09:48:06.375Z">
           <TotalTimeSeconds>6.0</TotalTimeSeconds>
           <DistanceMeters>15.5</DistanceMeters>
@@ -81,64 +82,83 @@ class ParserSpec extends FreeSpec with Matchers {
         "Manual",
         Seq.empty)
 
-      laps.size shouldBe 1
-      laps.head shouldEqual expected
+      laps shouldEqual Success(expected)
     }
 
     "Activity is parsed successfully" in {
-      val tcd = parse(
-        <TrainingCenterDatabase>
-          <Activities>
-            <Activity Sport="Running">
-              <Id>2016-04-30T09:38:57.000Z</Id>
-              <Lap StartTime="2016-04-30T09:38:57.000Z">
-                <TotalTimeSeconds>549.0</TotalTimeSeconds>
-                <DistanceMeters>1277.699951171875</DistanceMeters>
-                <MaximumSpeed>9.79999828338623</MaximumSpeed>
-                <Calories>1467</Calories>
-                <AverageHeartRateBpm>
-                  <Value>140</Value>
-                </AverageHeartRateBpm>
-                <MaximumHeartRateBpm>
-                  <Value>151</Value>
-                </MaximumHeartRateBpm>
-                <Intensity>Active</Intensity>
-                <TriggerMethod>Manual</TriggerMethod>
-                <Track>
-                  <Trackpoint>
-                    <Time>2016-04-30T09:38:58.000Z</Time>
-                    <AltitudeMeters>3.0</AltitudeMeters>
-                    <DistanceMeters>100.0</DistanceMeters>
-                    <HeartRateBpm>
-                      <Value>87</Value>
-                    </HeartRateBpm>
-                    <SensorState>Present</SensorState>
-                  </Trackpoint>
-                </Track>
-              </Lap>
-            </Activity>
-          </Activities>
-        </TrainingCenterDatabase>)
+      val activity = parseActivity(
+        <Activity Sport="Running">
+          <Id>2016-04-30T09:38:57.000Z</Id>
+          <Lap StartTime="2016-04-30T09:38:57.000Z">
+            <TotalTimeSeconds>549.0</TotalTimeSeconds>
+            <DistanceMeters>1277.699951171875</DistanceMeters>
+            <MaximumSpeed>9.79999828338623</MaximumSpeed>
+            <Calories>1467</Calories>
+            <AverageHeartRateBpm>
+              <Value>140</Value>
+            </AverageHeartRateBpm>
+            <MaximumHeartRateBpm>
+              <Value>151</Value>
+            </MaximumHeartRateBpm>
+            <Intensity>Active</Intensity>
+            <TriggerMethod>Manual</TriggerMethod>
+            <Track>
+              <Trackpoint>
+                <Time>2016-04-30T09:38:58.000Z</Time>
+                <AltitudeMeters>3.0</AltitudeMeters>
+                <DistanceMeters>100.0</DistanceMeters>
+                <HeartRateBpm>
+                  <Value>87</Value>
+                </HeartRateBpm>
+                <SensorState>Present</SensorState>
+              </Trackpoint>
+            </Track>
+          </Lap>
+        </Activity>)
 
-      tcd.activities.size shouldBe 1
-      tcd.activities.head.id.toString shouldBe "2016-04-30T09:38:57.000Z"
+      activity.map(_.id.toString) shouldBe Success("2016-04-30T09:38:57.000Z")
     }
 
     "file is parsed successfully" in {
       val tcd = parse(getClass.getResource("/test.xml").getPath)
 
-      tcd.activities.size shouldBe 1
-      tcd.activities.head.id.toString shouldBe "2016-04-30T09:38:57.000Z"
+      tcd.map(_.activities.size) shouldBe Success(1)
+      tcd.map(_.activities.head.id.toString) shouldBe Success("2016-04-30T09:38:57.000Z")
+    }
+  }
+
+  "sequence" - {
+    "flattens a sequence of successful tries in a try of a sequence" in {
+      val xs = Seq(Try(1), Try(2))
+
+      sequence(xs) shouldBe Try(Seq(1, 2))
+    }
+
+    "first exception in presence of failures" in {
+      val ouch = Failure(new Exception("ouch"))
+      val xs = Seq(
+        Try(1),
+        ouch,
+        Failure(new Exception("whoops")))
+
+      sequence(xs) shouldBe ouch
     }
   }
 
   "optionally" - {
-    "applies function to node, if it exists" in {
-      optionally(<A>1</A>)(_.text.toInt + 1) shouldBe Some(2)
+    "safely applies function to node, if it exists" in {
+      optionally(<A>1</A>)(_.text.toInt + 1) shouldBe Success(Some(2))
     }
 
-    "returns None when it doesn't exist" in {
-      optionally(NodeSeq.Empty)(_ => ()) shouldBe None
+    "returns a successful empty option, if node does not exist" in {
+      optionally(NodeSeq.Empty)(_ => ()) shouldBe Success(None)
+    }
+
+    "returns a failure, if something goes wrong with the function" in {
+      optionally(<A>X</A>)(_.text.toInt + 1) match {
+        case Failure(_) => succeed
+        case other => fail
+      }
     }
   }
 }
